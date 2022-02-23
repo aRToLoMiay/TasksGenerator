5a8a47ffe21f8fc6fc7ba02246b6e9033b309652
Artsiom Aulas <aRToLoMiay@tut.by>
2022-02-23 16:25:33 +0300

    Added project files.

---
+++ src/main.cpp
@@ -0,0 +1,81 @@
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+#include <qdebug.h>
+
+void genTasks(int n);
+void getMultTasks(int n);
+
+double drand(double min, double max) {
+    return (double)rand() / RAND_MAX * (max - min) + min;
+}
+
+double dround(double val, int n) {
+    double coef = pow(10, n);
+    return round(val * coef) / coef;
+}
+
+int main() {
+    int n = 25;
+    genTasks(n);
+    //getMultTasks(n);
+    return 0;
+}
+
+void genTasks(int n) {
+    srand(static_cast<unsigned int>(time(0)));
+
+    double* res = new double[n];
+    
+    double d = 1000;
+    for (int i = 0; i < n; i++) {
+        //int k = rand() * 3 / RAND_MAX + 1;
+        int k = 3;
+        double a = dround(drand(-d, d), k);
+        double b = dround(drand(-d, d), k);
+        int op = rand() * 2 / RAND_MAX + 1;
+
+        if (op == 1) {
+            res[i] = a + b;
+            qDebug() << "1. $$" << a << " + " << b << "$$";
+        }
+        else {
+            res[i] = a - b;
+            qDebug() << "1. $$" << a << " - " << b << "$$";
+        }
+    }
+    qDebug() << "=====";
+    for (int i = 0; i < n; i++) {
+        qDebug() << "1. " << res[i];
+    }
+}
+
+void getMultTasks(int n) {
+    srand(static_cast<unsigned int>(time(0)));
+
+    double* res = new double[n];
+
+    double d = 10;
+    for (int i = 0; i < n; i++) {
+        //int k = rand() * 3 / RAND_MAX + 1;
+        int k = 1;
+        double a = dround(drand(-d, d), k);
+        double b = dround(drand(-d, d), k);
+        //int op = rand() * 2 / RAND_MAX + 1;
+        int op = 1;
+
+        if (op == 1) {
+            res[i] = a * b;
+            qDebug() << "1. $$" << a << " \\cdot " << b << "$$";
+        }
+        else {
+            res[i] = a;
+            a = a * b;
+            qDebug() << "1. $$" << a << " / " << b << "$$";
+        }
+    }
+    qDebug() << "=====";
+    for (int i = 0; i < n; i++) {
+        qDebug() << "1. " << res[i];
+    }
+}
